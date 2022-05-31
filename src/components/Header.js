9c3baaf9bbb663cc2b7d6788ab6efeeb3e62204c
amrita <priyaamrita@5gmail.com>
2022-05-31 13:24:37 +0530

    finalcommit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = ({totalIncome}) => {
+  return (
+    <header>
+      <h1>Income Tracker</h1>
+      <div className="total-income">£{totalIncome}</div>
+    </header>
+  );
+};
+
+export default Header;
+
+
